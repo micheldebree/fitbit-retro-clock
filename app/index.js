9c700c6b95b2a052ec1936ef157cfc5f700e7bfa
Michel de Bree <michel@micheldebree.nl>
2019-01-03 20:52:24 +0100

    First working version

--- app/index.js
+++ app/index.js
@@ -2,18 +2,16 @@ import clock from 'clock';
 import document from 'document';
 import { preferences } from 'user-settings';
 
-import * as util from '../common/utils';
-
 clock.granularity = 'minutes';
 
-const elTime = document.getElementById('time');
-const elDay = document.getElementById('day');
-const elDayOfWeek = document.getElementById('dow');
-const elMonth = document.getElementById('month');
+const elH1 = document.getElementById('h1');
+const elH2 = document.getElementById('h2');
+const elM1 = document.getElementById('m1');
+const elM2 = document.getElementById('m2');
 
 function updateClock() {
   const now = new Date();
-  const minutes = util.zeroPad(now.getMinutes());
+  const minutes = now.getMinutes();
   let hours = now.getHours();
 
   if (preferences.clockDisplay === '12h') {
@@ -21,10 +19,10 @@ function updateClock() {
     hours = hours || 12;
   }
 
-  elTime.text = `${util.monoDigits(hours)}:${util.monoDigits(minutes)}`;
-  elDay.text = `${now.getDate()}`;
-  elDayOfWeek.text = `${util.getShortDayName(now.getDay())}`;
-  elMonth.text = `${util.getShortMonthName(now.getMonth())}`;
+  elH1.image = `${Math.floor(hours / 10)}.png`;
+  elH2.image = `${hours % 10}.png`;
+  elM1.image = `${Math.floor(minutes / 10)}.png`;
+  elM2.image = `${minutes % 10}.png`;
 }
 
 clock.ontick = () => updateClock();
